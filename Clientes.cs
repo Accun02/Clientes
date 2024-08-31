0f0c4f8b397aecdda0f272a0b13c1c4d4fc2c6e2
Accun02 <fhermopelufo@uade.edu.ar>
2024-08-31 12:18:43 -0300

    Inicial

---
+++ Clientes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clientes
+{
+   class Clientes
+    {
+        
+        string name;
+        string surname;
+        string phone;
+        float spending;
+        int buys;
+        
+
+
+
+        void start()
+        {
+           
+        }
+    }
+
+   
+
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Clientes
+{
+    internal class Program
+    {
+
+       static public Dictionary<int, Clientes> Id;
+        static void Main(string[] args)
+        {
+            
+            void start () 
+            {
+               
+            }
+            void Update()
+            {
+                
+            }
+        }
+    }
+}
